36fe9d642ea1fede9964a2db737d1d8565888130
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-05 20:54:12 +0900

    add: react-query QueryClientProvider in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div className="App"></div>;
+  <QueryClientProvider client={queryClient}>
+    return <div className="App"></div>;
+  </QueryClientProvider>;
 }
 
 export default App;
