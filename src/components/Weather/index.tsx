347d5d5edd3e3f3fd679d52467d3ca806e328e1f
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-06 17:14:45 +0900

    add: Weather Component UI logic and styles

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,9 +1,27 @@
 import useWeather from "../../hooks/useWeather";
+import { composeIconURL } from "../../network/weather";
+
+import "./index.css";
 
 const Weather = () => {
   const { data } = useWeather();
 
-  return <div>{data?.main.humidity}</div>;
+  if (data === undefined) return null;
+
+  const { main, weather } = data;
+
+  return (
+    <section className="weather">
+      <h1 className="weather__title">{weather[0].main}</h1>
+      <p className="weather__description">{weather[0].description}</p>
+      <img
+        className="weather__icon"
+        alt={weather[0].main}
+        src={composeIconURL(weather[0].icon)}
+      />
+      <span className="weather__text--temp">{`${main.temp} °`}</span>
+    </section>
+  );
 };
 
 export default Weather;
