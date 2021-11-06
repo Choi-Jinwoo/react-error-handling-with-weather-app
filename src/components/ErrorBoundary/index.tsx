b4d39bb7d15ae221acc95626e93cdb1c66f10b08
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-06 13:19:35 +0900

    add: ErrorBoundary component to handle error

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+type Props = {
+  fallback: React.Component;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: unknown) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
